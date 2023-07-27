f5ed000fd8c722bc5b52f97d2ab19e577cfbcb01
adanglingpointer <dave@imail.cloud>
2023-07-26 21:50:12 -0400

    add basic validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,28 +1,41 @@
-import { useRef, useState } from 'react';
+import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
-  const [enteredName, setEnteredName] = useState('');
+  const [enteredName, setEnteredName] = useState("");
 
-  const nameInputChangeHandler = event => {
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-  }
+  };
 
-  const formSubmissionHandler = event => {
+  const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    if (enteredName.trim() == "") {
+      return;
+    }
 
     console.log("Holding value with state: " + enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log("Holding value with ref: " + enteredValue);
-  }
+
+    // nameInputRef.current.value = ''; >> although this may work, not ideal
+    setEnteredName("");
+  };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
-      <div className='form-actions'>
+      <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
